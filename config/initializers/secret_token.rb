b43067f1b93e7fdd15059fa99648fb140d09b06e
Masashi Kishikawa <masashi@cs.wisc.edu>
2014-02-09 00:25:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CassetteUp::Application.config.secret_key_base = 'caab236fd80f4d8e20a37a31929709141ad8723bede3532b5e535d0f6f077c5e21cdbaab642d04540dadd556f0d9a2b5d3b277f9a4b663c139739cf3c9a72c90'
